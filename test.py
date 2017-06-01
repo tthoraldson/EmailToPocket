baf05f4f839b9de2fc139e870f816379ccc36e13
Theresa Thoraldson <theresa.thoraldson@outlook.com>
2017-06-01 10:58:53 -0500

    Gmail library installed, the struggles continue

--- test.py
+++ test.py
@@ -3,11 +3,15 @@ from gmail import Gmail
 
 username = 'theresa.thoraldson@gmail.com'
 
+# password
 print('Please enter the password for ' + username)
 password = raw_input()
 
-print('The password you entered is: ' + password)
+g = Gmail()
+g.login(username, password)
 
-# g = gmail.login(username, password)
+g.inbox().mail(sender="noreply@medium.com")
 
+# cleanup, logout, and exit
+g.logout()
 sys.exit()
