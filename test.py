19b571de93e83bf1d4107a29b6f4177a1cb1cfd3
Theresa Thoraldson <theresa.thoraldson@outlook.com>
2017-05-31 19:51:31 -0500

    the struggles

---
+++ test.py
@@ -0,0 +1,13 @@
+import sys
+from gmail import Gmail
+
+username = 'theresa.thoraldson@gmail.com'
+
+print('Please enter the password for ' + username)
+password = raw_input()
+
+print('The password you entered is: ' + password)
+
+# g = gmail.login(username, password)
+
+sys.exit()
